5ee38bbd4f13e336248b55437fb4c94c4c3cf91f
soya.miruku <lizodesignergfx@gmail.com>
2023-12-07 10:58:47 +0000

    update meta thing

--- src/functions/meta.ts
+++ src/functions/meta.ts
@@ -1,6 +1,7 @@
 import type { Constructor } from 'type-fest';
 import { qlFn } from './index.ts'
 import { IModel } from '../types/types.ts';
+import { type } from './mod.ts';
 
 export function id(record: `${string}:${string}`) {
   return qlFn.create(`meta::id(${record})`)
@@ -10,7 +11,7 @@ export function tb(record: `${string}:${string}`) {
 }
 
 export function thing<T extends Constructor<IModel>, Idx extends string>(target: T, id: Idx) {
-  return qlFn.create(`${target.name}:${id}`);
+  return type.thing(target.name, id)
 }
 
 export function tbl<T extends Constructor<IModel>>(target: T) {
